896f017ca4092c4ba946d6f1c10693c92da12e81
LopatoEO <126246079+LopatoEO@users.noreply.github.com>
2026-06-12 18:14:54 +0300

    f

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-app = FastAPI(title='FastAPI application for demonstration purposes')
+app = FastAPI(title='FastAPI application for demonstration purposes fhdskfjbsdjkfbjksdbfjsbdjfbdjsbfljsbdfjbsdjkfbjksdbfjbdsfjbdjlbfkjsbdflbsdkbfjlbfjbsdfljbsdkfb jdsbfosdbfmdsbfjsbdfosdhbf')
 
 @app.get("/")
 async def root():
