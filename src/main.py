cef7242f0efc8c3cca8ed552ae25fa3e8dda6cea
LopatoEO <126246079+LopatoEO@users.noreply.github.com>
2026-06-13 15:45:24 +0300

    Monitoring

--- src/main.py
+++ src/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
+from prometheus_client import Counter
 
 app = FastAPI(title='FastAPI application for demonstration purposes')
+
+multiply_operations = Counter(
+    "multiply_operations_total",
+    "Number of multiply operations"
+)
+
+Instrumentator().instrument(app).expose(app)
 
 @app.get("/")
 async def root():
@@ -8,4 +17,5 @@ async def root():
 
 @app.get("/actions/multiply")
 async def multiply(a: int, b: int):
+    multiply_operations.inc()
     return {"result": a * b}
